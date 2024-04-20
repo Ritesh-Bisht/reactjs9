eedf556f9ed896b6677f1d4eef9083b35facf39d
Ritesh Bisht <rb1342003@gmail.com>
2024-04-20 19:40:23 +0530

    custom react start

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
-
+function MYAPP(){
+  return(
+    <div> 
+      <h1> CUSTOM APP ! CHAI   </h1>
+    </div>
+  )
+}
+const reactElement= React.createElement(
+  'a',
+  {href:'http://localhost:5173/', target:'_blank'},
+  'Click here to visit website'
+)
 ReactDOM.createRoot(document.getElementById('root')).
 render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+     reactElement
+ 
 )
