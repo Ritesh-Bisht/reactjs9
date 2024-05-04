cf84c9d9b7de6651e6d87faf1164785d4c3ea49d
Ritesh Bisht <rb1342003@gmail.com>
2024-05-04 19:56:18 +0530

    reduxToolkitTodo 2

--- reduxToolkitTodo/src/app/store.js
+++ reduxToolkitTodo/src/app/store.js
@@ -1,6 +1,6 @@
-import {configureStore} from '@reduxjs/toolkit'
-import todoReducer from '../features/todo/todoSlice'
-export default store = configureStore({
+import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../features/todo/todoSlice';
+
+export const store = configureStore({
     reducer: todoReducer
-    
 })
