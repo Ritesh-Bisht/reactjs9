231bbcd88158ce16c3239d04f6aaf00facb1765b
Ritesh Bisht <rb1342003@gmail.com>
2024-05-09 20:00:39 +0530

    index.js

--- 12MegaBlog/src/components/header/header.jsx
+++ 12MegaBlog/src/components/header/header.jsx
@@ -1,8 +1,38 @@
 import React from "react";
+import {Container , Logo , LogoutBtn} from "../index"
+import {Link} from "react-router-dom"
+import {useSelector} from 'react-redux'
+import { useNavigate } from "react-router-dom";
+
+
 
 function Header(){
+    const authStatus=useSelector((state)=>state.auth.status);
+    const navigate = useNavigate()
+    const naItems = [
+        {
+            name:'Home',
+            slug:"/",
+            active:true,
+        },
+        {
+            
+        },
+        {
+
+        },
+        {
+
+        },
+    ]
     return(
-        <div>HEADER</div>
+        <header>
+            <Container>
+                <nav>
+                    <div className=""></div>
+                </nav>
+            </Container>
+        </header>
     )
 }
 export default Header;
