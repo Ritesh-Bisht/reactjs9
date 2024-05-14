f4e299ca5b7d66732fba4f9f48b3ef13b05a6264
Ritesh Bisht <rb1342003@gmail.com>
2024-05-14 19:47:47 +0530

    RTE.jsx created

---
+++ 12MegaBlog/src/components/Signup.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import AuthService from "../appwrite/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../store/authSlice";
+import {Button, Input, Logo} from "./index"
+
+function Signup(){
+    
+}
